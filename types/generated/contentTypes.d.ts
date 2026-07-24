4cdd05825912d2dadcc715dcaa03a03abb565809
FizaXYZ123 <Fiza@thexyzstudio.com>
2026-07-24 11:21:48 +0530

    populate clubOwner detail in login and remove isVerified

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1555,7 +1555,6 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 6;
       }>;
-    isVerified: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
